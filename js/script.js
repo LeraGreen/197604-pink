c455502975ca124907eaa6101cb7f81d941f954c
Green Lera <zelnayavaleriya@gmail.com>
2016-12-18 22:10:11 +0300

    project

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ var mainMenuWrapper = document.getElementById('main-menu-wrapper');
 
 mainMenuToggler.onclick = function(event) {
   event.preventDefault();
-  mainMenuToggler.classList.toggle('page-header__menu-toggler--close');
+  mainMenuToggler.classList.toggle('page-header__menu-toggler--close')
   mainMenuToggler.classList.toggle('page-header__menu-toggler--open');
   mainMenuWrapper.classList.toggle('page-header__top-line--open');
 }
+
+function closeMenu() {
+  if (mainMenuWrapper.classList.contains('page-header__top-line--open')) {
+    mainMenuWrapper.classList.remove('page-header__top-line--open');
+    mainMenuToggler.classList.remove('page-header__menu-toggler--close')
+  }
+}
+
+closeMenu();
 
 var map;
 function initMap() {
