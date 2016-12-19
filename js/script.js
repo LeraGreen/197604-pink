00596817ab278240fff6921ed00d361fdfe5c475
Green Lera <zelnayavaleriya@gmail.com>
2016-12-19 22:23:48 +0300

    доработка страниц

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ var mainMenuWrapper = document.getElementById('main-menu-wrapper');
 
 mainMenuToggler.onclick = function(event) {
   event.preventDefault();
-  mainMenuToggler.classList.toggle('page-header__menu-toggler--close')
-  mainMenuToggler.classList.toggle('page-header__menu-toggler--open');
+  mainMenuToggler.classList.toggle('page-header__menu-toggler--close');
   mainMenuWrapper.classList.toggle('page-header__top-line--open');
 }
 
