93a53e2146b5bd34bf5f2c4b22cfd56f4a195953
Green Lera <zelnayavaleriya@gmail.com>
2016-12-20 22:26:18 +0300

    работа над замечаниями

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var mainMenuWrapper = document.getElementById('main-menu-wrapper');
 mainMenuToggler.onclick = function(event) {
   event.preventDefault();
   mainMenuToggler.classList.toggle('page-header__menu-toggler--close');
+  mainMenuToggler.classList.toggle('page-header__menu-toggler--open');
   mainMenuWrapper.classList.toggle('page-header__top-line--open');
 }
 
