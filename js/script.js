6d88e0239bce1c8eb71346302b4c943e33acbc1b
Green Lera <zelnayavaleriya@gmail.com>
2016-12-25 09:17:33 +0300

    двойные кавычки в js файле для

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-var mainMenuToggler = document.getElementById('main-menu-toggler');
-var mainMenuWrapper = document.getElementById('main-menu-wrapper');
+var mainMenuToggler = document.getElementById("main-menu-toggler");
+var mainMenuWrapper = document.getElementById("main-menu-wrapper");
 
 mainMenuToggler.onclick = function(event) {
   event.preventDefault();
-  mainMenuToggler.classList.toggle('page-header__menu-toggler--close');
-  mainMenuToggler.classList.toggle('page-header__menu-toggler--open');
-  mainMenuWrapper.classList.toggle('page-header__top-line--open');
+  mainMenuToggler.classList.toggle("page-header__menu-toggler--close");
+  mainMenuToggler.classList.toggle("page-header__menu-toggler--open");
+  mainMenuWrapper.classList.toggle("page-header__top-line--open");
 }
 
 function closeMenu() {
-  if (mainMenuWrapper.classList.contains('page-header__top-line--open')) {
-    mainMenuWrapper.classList.remove('page-header__top-line--open');
-    mainMenuToggler.classList.remove('page-header__menu-toggler--close')
+  if (mainMenuWrapper.classList.contains("page-header__top-line--open")) {
+    mainMenuWrapper.classList.remove("page-header__top-line--open");
+    mainMenuToggler.classList.remove("page-header__menu-toggler--close")
   }
 }
 
@@ -22,7 +22,7 @@ closeMenu();
 var map;
 function initMap() {
   var map;
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 59.938536, lng: 30.3224549},
     zoom: 15
   });
@@ -33,11 +33,11 @@ function initMap() {
 
 function setMarkers(map) {
   var points = [
-    ['Pink', 59.938536, 30.3224549],
+    ["Pink", 59.938536, 30.3224549],
   ];
 
   var image = {
-    url: 'img/icon-map-marker.svg',
+    url: "img/icon-map-marker.svg",
     size: new google.maps.Size(36, 36),
     anchor: new google.maps.Point(18, 18)
   };
